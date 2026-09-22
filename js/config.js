f9b43625bd55299c31d3eab375e1d35f67e6484d
disollet-sudo <disollet@gmail.com>
2026-09-22 10:23:32 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@
  */
 window.PV = window.PV || {};
 window.PV.config = {
-  API_URL: 'https://script.google.com/macros/s/AKfycbxzfxzNq2AmdAIODmQCSEDxeOPA9dnm5Es6CcOMXOBlE0VOPm1-AsyLOHhAkOJHyzuq/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbylkztc_TOvUEm_e70uD4B5nPo8SUuSGPep9QRRi081dmdCQiN_oxjRO1eJ0GZuOYsn/exec',
   REFRESH_MINUTES: 5, // atualiza sozinho enquanto a aba estiver aberta
 };
